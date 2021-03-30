08d9a9923c506f174022873216660db2f7af916a
Aoa0 <837575715@qq.com>
2021-03-30 17:46:29 +0800

    March Update

--- files/script.js
+++ files/script.js
@@ -47,6 +47,7 @@ var security_2021 = [
     ['https://www.usenix.org/conference/usenixsecurity22/call-for-papers', 'Security 2022', '2021-10-12', '2022-01-20', '13 +', 'Boston', 'USENIX'],
     ['https://esorics2021.athene-center.de/call-for-papers.php', 'ESORICS 2021', '2021-01-12', '2021-03-12', '16 +', 'Darmstadt', 'Springer'],
     ['https://esorics2021.athene-center.de/call-for-papers.php', 'ESORICS 2021', '2021-05-12', '2021-07-12', '16 +', 'Darmstadt', 'Springer'],
+    ['https://raid2021.org/call-for-papers/', 'RAID 2021', '2021-04-01', '2021-06-06', '12+', 'Donostia', 'ACM']
 
 
 ]
